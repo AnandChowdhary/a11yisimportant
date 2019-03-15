2c50f2e8d5928dd170def327be893c614fd3a0d1
Anand Chowdhary <mail@anandchowdhary.com>
2019-03-15 23:38:07 +0100

    :sparkles: Add support for following users

--- index.ts
+++ index.ts
@@ -9,15 +9,38 @@ const client = new Twitter({
   access_token_secret: <string>process.env.ACCESS_SECRET
 });
 
-import { SearchResult, Tweet, User } from "./interfaces";
+// @ts-ignore
+import serial from "promise-serial";
+import { uniqBy } from "lodash";
+import { SearchResult, User } from "./interfaces";
 
 const init = async () => {
   // Remove duplicates and filter users you don't follow
-  const people = [...new Set(await findPeople())].filter(
-    user => !user.following && !user.follow_request_sent
+  const people = uniqBy(
+    (await findPeople()).filter(
+      user => !user.following && !user.follow_request_sent
+    ),
+    "id"
   );
-  console.log("Got people", people.length);
+  // Follow each person in the list
+  console.log(`Following ${people.length} people`);
+  const promises = people.map(person => () =>
+    new Promise((resolve, reject) => {
+      follow(person)
+        .then(response => resolve(response))
+        .catch(error => reject(error));
+    })
+  );
+  return serial(promises);
 };
+
+const follow = async (person: User) =>
+  new Promise(resolve => {
+    client.post("friendships/create", { user_id: person.id }, () => {
+      console.log(`Followed ${person.name} (@${person.screen_name})`);
+      resolve();
+    });
+  });
 
 const recentTweets = async (hashTag: string) =>
   new Promise((resolve, reject) => {
