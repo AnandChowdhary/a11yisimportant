f2856a4075e958bf2a04a48659ddda40a7e779d7
Anand Chowdhary <mail@anandchowdhary.com>
2019-03-16 08:49:33 +0100

    :art: Move server imports to one file

--- follow.ts
+++
@@ -1,8 +0,0 @@
-import { init } from "./index";
-import { Response, Request } from "express";
-
-export default (req: Request, res: Response) => {
-  init()
-    .then(() => res.json({ followed: true }))
-    .catch(error => res.json({ error }));
-};
--- server.ts
+++ server.ts
@@ -1,10 +1,14 @@
-import express from "express";
+import express, { Response, Request } from "express";
 const app = express();
 
-import follow from "./follow";
+import { init } from "./index";
 
-app.get("/", (req, res) => res.json({ hello: "world" }));
-app.get("/follow", follow);
+app.get("/", (req: Request, res: Response) => res.json({ hello: "world" }));
+app.get("/follow", (req: Request, res: Response) => {
+  init()
+    .then(() => res.json({ followed: true }))
+    .catch(error => res.json({ error }));
+});
 
 app.listen(process.env.PORT || 7001, () =>
   console.log("@a11yisimportant is ready.")
